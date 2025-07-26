0537b3ae3f6b9714f893914415ee16413f0db25f
aldwinbucio <aldwinbucio@gmail.com>
2025-07-26 22:15:32 +0800

    Updated tsconfig.app.json

--- RECheck-main/vite.config.ts
+++ RECheck-main/vite.config.ts
@@ -1,27 +1,7 @@
-import path from "path"
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: [],
-      manifest: {
-        name: 'RECheck',
-        short_name: 'RECheck',
-        theme_color: '#ffffff',
-      },
-    })
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
 })
